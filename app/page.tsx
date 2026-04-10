00ad685e77fd0049dcd827844466df71b5831303
Pablo Dias <pabllosoarez@gmail.com>
2026-04-10 12:15:06 -0300

    feat: add logo image in WebP format to the public images directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
+import Image from 'next/image'
+
+import logo from '@/public/images/logo-colors.png'
+import Link from 'next/link'
+import {
+    CaretDownIcon,
+    MagnifyingGlassIcon,
+} from '@phosphor-icons/react/dist/ssr'
+
+const EXTERNAL_LINKS = [
+    { label: 'Somos Católica', href: 'https://www.catolica.edu.br/' },
+    { label: 'Biblioteca', href: 'https://biblioteca.catolica.edu.br/' },
+    { label: 'Notícias', href: '#' },
+    { label: 'Atendimento ao Aluno', href: '#' },
+    { label: 'Egresso', href: '#' },
+    { label: 'CPA', href: '#' },
+    { label: 'Ouvidoria', href: '#' },
+    { label: 'Canal de Conduta Ética', href: '#' },
+]
+
+const NAVBAR_LINKS = [
+    { label: 'A Católica', href: '#' },
+    { label: 'Cursos', href: '#' },
+    { label: 'Pesquisa', href: '#' },
+    { label: 'Extensão', href: '#' },
+    { label: 'Educadores', href: '#' },
+]
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    return (
+        <>
+            <div className="bg-neutral-50">
+                <div className="max-w-7xl mx-auto justify-end flex items-center">
+                    <ul className="flex gap-6 items-center py-2 text-neutral-600">
+                        {EXTERNAL_LINKS.map((link) => (
+                            <li key={link.href}>
+                                <a
+                                    className="text-sm hover:text-secondary"
+                                    href={link.href}
+                                >
+                                    {link.label}
+                                </a>
+                            </li>
+                        ))}
+                        <li>
+                            <button className="font-semibold text-sm hover:text-secondary flex gap-1 items-center">
+                                <span>PT-BR</span>
+                                <CaretDownIcon weight="bold" />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <header className="bg-white p-6">
+                <div className="max-w-7xl mx-auto flex items-center">
+                    <Image
+                        src={logo}
+                        width={200}
+                        height={39}
+                        alt="Universidade Católica de Brasília"
+                    />
+
+                    <nav className="ml-10">
+                        <ul className="flex gap-2 items-center">
+                            {NAVBAR_LINKS.map((link) => (
+                                <li key={link.href}>
+                                    <a
+                                        className="px-4 py-2 font-medium hover:text-secondary"
+                                        href={link.href}
+                                    >
+                                        {link.label}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                    <div className="flex-1"></div>
+                    <button className="text-xl p-2 hover:text-secondary cursor-pointer transition-colors">
+                        <MagnifyingGlassIcon weight="bold" />
+                        <span className="sr-only">Buscar</span>
+                    </button>
+                    <button className="group px-4 py-2 flex gap-1 items-center justify-center cursor-pointer hover:text-secondary transition-colors">
+                        <span className="font-medium">UCB Online</span>
+                        <CaretDownIcon
+                            className="group-hover:rotate-180 transition-transform"
+                            weight="bold"
+                        />
+                    </button>
+                    <Link
+                        className="px-4 py-2 bg-primary text-white font-semibold rounded hover:bg-secondary ml-auto transition-colors"
+                        href={'#'}
+                    >
+                        Inscreva-se
+                    </Link>
+                </div>
+            </header>
+            <main>
+                <section>
+                    <video
+                        src="https://ucb.catolica.edu.br/hubfs/Cat%C3%B3lica%202023%20-%20MEC%20V10%20-%20WIDE%20(full%20HD%20-31%2c1mb).mp4
+"
+                        muted
+                        loop
+                        autoPlay
+                    ></video>
+                </section>
+            </main>
+        </>
+    )
 }
